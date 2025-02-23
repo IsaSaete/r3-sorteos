b56ef01e7c96386b2b5ee30831e8b827479d2b1c
Isabel Sáenz <isasaete@gmail.comn>
2025-02-23 08:49:03 +0100

    Complete task 1

--- src/giveaways.ts
+++ src/giveaways.ts
@@ -3,3 +3,18 @@ import { programData, saveData } from "./storage.js";
 
 import { Giveaway } from "./types.js";
 import { askUserNewGiveawayData } from "./ui.js";
+export const loginUser = (email: string, password: string): void => {
+  const validateLogin = programData.users.find(
+    (user) => user.email === email && user.password === password
+  );
+  if (!validateLogin) {
+    console.log("Error: No existe un usuario con estos datos");
+    process.exit();
+  }
+  if (validateLogin) {
+    programData.userEmail = validateLogin.email;
+    programData.isAdmin = validateLogin.isAdmin;
+  }
+  saveData();
+  console.log("Usuario registrado con exito");
+};
